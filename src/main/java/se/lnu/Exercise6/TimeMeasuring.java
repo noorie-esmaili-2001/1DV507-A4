3aeef57301f1d9d7631865eaea544ba340e33d5d
Noorie Esmaili <noorie.esmaili@gmail.com>
2024-02-04 13:24:53 +0100

    fix time measuring

---
+++ src/main/java/se/lnu/Exercise6/TimeMeasuring.java
@@ -0,0 +1,41 @@
+package se.lnu.Exercise6;
+
+import java.util.ArrayList;
+
+public class TimeMeasuring {
+
+    public static void main(String[] args) {
+        for (int i = 0; i < 1; i++) {
+
+            StringBuilder str = new StringBuilder();
+            long start = System.currentTimeMillis();
+            System.out.println(System.currentTimeMillis() - start < 1000);
+            while (System.currentTimeMillis() - start < 1000) {
+                str.append("#####");
+            }
+            long end = System.currentTimeMillis();
+            int runTime = (int) (end - start);
+
+            long begin = System.currentTimeMillis();
+            str.toString();
+            long finish = System.currentTimeMillis();
+            int toStringTime = (int) (finish - begin);
+
+            StringBuilder newBuilder = new StringBuilder();
+            long startPoint = System.currentTimeMillis();
+            while (System.currentTimeMillis() - startPoint < 1000 - toStringTime) {
+                newBuilder.append("#####");
+            }
+            long endPoint = System.currentTimeMillis();
+            int newRunTime = (int) (endPoint - startPoint);
+
+            System.out.println("Before ToString:  Approximate time: " + runTime + " milliseconds."
+                    + " Total concatenations: " + str.length() + ".  Total length: " + str.length());
+            System.out.println("ToString time: " + toStringTime + " milliseconds.");
+            System.out.println("After ToString:  Approximate time: " + (newRunTime + toStringTime)
+                    + " milliseconds.  Total concatenation: " + (newBuilder.length() / 80) + ".  Total Length: "
+                    + newBuilder.length() + "\n");
+        }
+
+    }
+}
